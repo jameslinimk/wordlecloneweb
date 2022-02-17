145bcbef33bb1444af83e38347efb9dd30810bc8
Linimik <jameskimimk@gmail.com>
2022-02-17 07:29:44 -0500

    Changed instantpopups css

--- src/App.svelte
+++ src/App.svelte
@@ -316,6 +316,7 @@
 
 		display: flex;
 		flex-direction: column;
+		align-items: center;
 	}
 
 	.githubIco {
--- src/componenets/instantpopup.svelte
+++ src/componenets/instantpopup.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { fade } from "svelte/transition";
+
     export let message: string;
     export let duration: number;
     export let destroy: () => void;
@@ -6,7 +8,7 @@
     setTimeout(destroy, duration);
 </script>
 
-<div class="instantpopup">
+<div class="instantpopup" in:fade out:fade>
     {message}
 </div>
 
@@ -15,6 +17,7 @@
         text-align: center;
         font-size: x-large;
         font-weight: bold;
+        width: fit-content;
 
         background-color: #fff;
         border-radius: 5px;
